c9a1756287f28bafa8b3aac4c62767ed96bb1c3c
Burak Kuyucu <kuyucuburakg@gmail.com>
2022-05-29 19:06:01 +0300

    first commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public void StartFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
